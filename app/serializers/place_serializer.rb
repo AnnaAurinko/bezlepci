31d9c601eec4d71c47b1a0803fd351a1fe6d4425
MariDani <danielova.mariana@gmail.com>
2014-06-01 10:53:24 +0200

    categories of places on the map

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -12,13 +12,56 @@ class PlaceSerializer < ActiveModel::Serializer
   end
 
   def properties
-    {
+    case object.specification
+    when "beer"
+      {
       name: object.name,
       address: object.address,
       description: object.description,
-      "marker-color" => "#BF2E57",
-      "marker-symbol" => "dog-park",
+      "marker-color" => "#2F7582",
+      "marker-symbol" => "beer",
+      "marker-size" => "medium"
+      }
+
+    when "shop"
+        {
+        name: object.name,
+        address: object.address,
+        description: object.description,
+        "marker-color" => "#C86F73",
+        "marker-symbol" => "shop",
+        "marker-size" => "medium"
+      }
+
+    when "coffee"
+        {
+        name: object.name,
+        address: object.address,
+        description: object.description,
+        "marker-color" => "#725421",
+        "marker-symbol" => "cafe",
+        "marker-size" => "medium"
+      }
+
+    when "Restaurace"
+      {
+      name: object.name,
+      address: object.address,
+      description: object.description,
+      "marker-color" => "#717135",
+      "marker-symbol" => "restaurant",
       "marker-size" => "medium"
     }
+
+    # missing specification
+    else 
+     {
+      name: object.name,
+      address: object.address,
+      description: object.description,
+      "marker-color" => "#FAAD1E",
+      "marker-size" => "medium"
+    }
+    end
   end
 end
